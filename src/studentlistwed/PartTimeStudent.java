4133f52d41b2a3503701ea1c941a7dc6b817b979
ksaqui <saquika@sheridancollege.ca>
2023-06-19 23:31:02 -0400

    New class: PartTimeStudent

---
+++ src/studentlistwed/PartTimeStudent.java
@@ -0,0 +1,13 @@
+package studentlistwed;
+
+/**
+ * Class that models a part-time student
+ * @author saquika, June 2023
+ */
+public class PartTimeStudent extends Student{
+    
+    public PartTimeStudent(String givenName) {
+        super(givenName);
+    }
+    
+}
